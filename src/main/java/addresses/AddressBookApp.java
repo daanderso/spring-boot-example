f2aba8d8e937f2ecc13613ae9a8b72593420b769
Derek <daanderso@gmail.com>
2017-06-14 22:50:09 -0400

    Spring Boot Exercise

---
+++ src/main/java/addresses/AddressBookApp.java
@@ -0,0 +1,13 @@
+package addresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApp.class, args);
+    }
+
+}
